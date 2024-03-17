bef83f1493faa6740c389b2f4865afcb56d54480
Nataliia Holub <golub.togo@gmail.com>
2024-03-17 20:47:37 +0100

    create Car class

---
+++ src/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+
+    public void start(){
+        startElectricity();
+        startCommand();
+        startFuelSystem();
+    }
+
+    private void startElectricity() {
+        System.out.println("Start electricity.");
+    }
+
+    private void startCommand() {
+        System.out.println("Start command.");
+    }
+
+    private void startFuelSystem() {
+        System.out.println("Start fuel system.");
+    }
+}
